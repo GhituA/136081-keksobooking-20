ae02d3578ac3fe7f30cb34a2ff687a73fed370ca
GhituA <isthatpawz@gmail.com>
2020-07-22 16:17:31 +0300

    Добавляет отображение превью при загрузке изображений

--- js/form.js
+++ js/form.js
@@ -5,16 +5,21 @@
   var MIN_TITLE_LENGTH = 30;
   var MAX_TITLE_LENGTH = 100;
   var MAX_PRICE = 1000000;
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   var adForm = document.querySelector('.ad-form');
   var resetButton = adForm.querySelector('.ad-form__reset');
-  var roomField = adForm.querySelector('#room_number');
-  var capacityField = adForm.querySelector('#capacity');
+
+  var avatarField = adForm.querySelector('#avatar');
+  var avatarPreview = adForm.querySelector('.ad-form-header__preview img');
   var titleField = adForm.querySelector('#title');
   var typeField = adForm.querySelector('#type');
   var priceField = adForm.querySelector('#price');
   var timeinField = adForm.querySelector('#timein');
   var timeoutField = adForm.querySelector('#timeout');
+  var roomField = adForm.querySelector('#room_number');
+  var capacityField = adForm.querySelector('#capacity');
+  var imagesField = adForm.querySelector('#images');
 
   var onUpload = window.load.upload;
   var renderMessage = window.message.render;
@@ -82,7 +87,6 @@
     setMainPinCoordinates(MAIN_PIN_HEIGHT_ACTIVE);
   };
 
-
   var onSubmitSuccess = function () {
     renderMessage(messageMap.success);
     onFormReset();
@@ -93,17 +97,38 @@
     onUpload(new FormData(adForm), onSubmitSuccess, renderMessage.bind(renderMessage, messageMap.error));
   };
 
-  roomField.addEventListener('change', function (evt) {
-    var rooms = evt.target.value;
-    for (var j = 0; j < capacityField.options.length; j++) {
-      capacityField.options[j].disabled = !capacityMap[rooms].guests.includes(capacityField.options[j].value);
-    }
-    checkCapacityValidity();
-  });
+  var onFileChange = function (inputField, previewField) {
+    var file = inputField.files[0];
+    var fileName = file.name.toLowerCase();
 
-  capacityField.addEventListener('change', function () {
-    checkCapacityValidity();
-  });
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var imgReader = new FileReader();
+
+      imgReader.addEventListener('load', function () {
+        previewField.src = imgReader.result;
+      });
+
+      imgReader.readAsDataURL(file);
+    }
+  }
+
+  var renderImage = function () {
+    var imagePreview = adForm.querySelector('.ad-form__photo');
+    var image = document.createElement('img');
+    image.className = 'ad-form__photo--preview';
+    image.style.width = '70px';
+    image.style.height = '70px';
+
+    onFileChange(imagesField, image);
+
+    imagePreview.appendChild(image);
+  };
+
+  avatarField.addEventListener('change', onFileChange.bind(onFileChange, avatarField, avatarPreview));
 
   titleField.addEventListener('input', function (evt) {
     var valueLength = evt.target.value.length;
@@ -133,6 +158,20 @@
   timeoutField.addEventListener('change', function (evt) {
     timeinField.value = evt.target.value;
   });
+
+  roomField.addEventListener('change', function (evt) {
+    var rooms = evt.target.value;
+    for (var j = 0; j < capacityField.options.length; j++) {
+      capacityField.options[j].disabled = !capacityMap[rooms].guests.includes(capacityField.options[j].value);
+    }
+    checkCapacityValidity();
+  });
+
+  capacityField.addEventListener('change', function () {
+    checkCapacityValidity();
+  });
+
+  imagesField.addEventListener('change', renderImage);
 
   resetButton.addEventListener('click', onFormReset);
   resetButton.addEventListener('keydown', function (evt) {
